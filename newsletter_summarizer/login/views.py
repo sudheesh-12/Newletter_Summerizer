92ab57f0ed0e71817b49a719c69fe9d7dfe06efd
Sudheesh_07 <sudheeshshetty48@gmail.com>
2024-10-01 17:09:18 +0530

    integrated the login page

---
+++ newsletter_summarizer/login/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Logged in successfully!')
+        else:
+            return HttpResponse('Invalid credentials')
+
+    return render(request, 'login/login.html')
