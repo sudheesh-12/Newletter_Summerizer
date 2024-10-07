f0cb7b0cbf8ebe7676cb63262892104718026ca1
Harish-tig <nadarharish03@gmail.com>
2024-10-07 22:26:55 +0530

    changed scrape views
    read comment

--- newsletter_summarizer/Scrape/views.py
+++ newsletter_summarizer/Scrape/views.py
@@ -5,23 +5,39 @@ import requests as Req , bs4
 
 
 # Create your views here.
+
 def head_function(request):
     url = "https://www.bbc.com/"
     response = Req.get(url).text
-    soup = bs4.BeautifulSoup(response,"html.parser")
-    headings = soup.find_all("h2")
-    paragraphs = soup.find_all("p")
-    img = soup.find_all("img")
-    sourse = [x.get("src") for x in img]
-    image_urls = sourse[1::2]
-    headlines = [heading.getText(strip=True) for heading in headings]
-    paragraphs_list = [para.getText(strip=True) for para in paragraphs]
-    news = zip(headlines,paragraphs_list,image_urls) #zip refer google or tuple unpacking
+    soup = bs4.BeautifulSoup(response, "html.parser")
+    tag_a = soup.select('a')
+    head = []
+    para = []
+    image = []
+    for elements in tag_a:
+        h2 = elements.select("h2")
+        p = elements.select("p")
+        img = elements.select('img[src]')
+        if h2 and p:
+            head.append(h2[0].getText())
+            para.append(p[0].getText())
+            if img:
+                image.append(img[1]['src'])
+            else:
+                image.append(r"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg")
+    news = zip(head,para,image)
     context = {
-        "news_items": news,
-        # "image": image_urls
+        "news_items":news
     }
+
     return render(request
                   ,template_name="home.html"
                   ,context=context)
+
+
+
+
+
+
+
 
